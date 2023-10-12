c16fbab2c849d823b1c398b1b7fabfee97036df2
Javier-201906795 <88503776+Javier-201906795@users.noreply.github.com>
2023-10-12 16:23:39 -0600

    Tk input texto
    se creo el cuadro donde se ingresaran las modificacion al archivo de entrada para el programa

--- Tk_ventana.py
+++ Tk_ventana.py
@@ -7,7 +7,7 @@ from tkinter import filedialog, messagebox
 ########################################################################
 raiz = Tk()
 raiz.title('LFP Proyecto 2 | 201906795')
-raiz.geometry('1000x800')
+raiz.geometry('1150x600')
 
 Label(raiz, pady=0,text='PROYECTO 2 | 201906795', font=("Consolas",14)).place(x=50,y=20)
 Button(raiz,text='Abrir', bg='#DBE6A1', fg='#000000',font=("Consolas",12)).place(x=500,y=20, width=90, height= 35)
@@ -32,6 +32,20 @@ def seleccion(event):
         print('Salir')
 #Ejecuta la opcion cuando cambia de seleccion
 listaopciones.bind('<<ComboboxSelected>>',seleccion)
+
+
+inputtexto = Text(raiz, padx=40)
+inputtexto.place(x=10,y=80, width=650, height=500)
+
+labelnumeracion = Label(raiz, pady=0,text=' 1.\n 2.\n 3.\n 4.\n 5.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=82)
+labelnumeracion = Label(raiz, pady=0,text=' 6.\n 7.\n 8.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=160)
+labelnumeracion = Label(raiz, pady=0,text=' 9.\n10.\n11.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=210)
+labelnumeracion = Label(raiz, pady=0,text='12.\n13.\n14.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=258)
+labelnumeracion = Label(raiz, pady=0,text='15.\n16.\n17.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=305)
+labelnumeracion = Label(raiz, pady=0,text='18.\n19.\n20.\n21.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=355)
+labelnumeracion = Label(raiz, pady=0,text='22.\n23.\n24.\n25.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=420)
+labelnumeracion = Label(raiz, pady=0,text='26.\n27.\n28.\n29.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=480)
+labelnumeracion = Label(raiz, pady=0,text='30.\n31.', font=("Consolas",10), bg="#FFFFFF").place(x=12,y=545)
 
 
 
